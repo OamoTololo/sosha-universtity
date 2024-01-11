f01c5882075c5f192c18915b7970158c9decd2f3
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-01-11 03:42:41 +0200

    Added bootstrap and fontawesome dependencies and my own stylesheet.

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -6,8 +6,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fontawesome.css">
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
-    <h1>This is home.</h1>
+    <div class="container-fluid">
+        <h1><i class="fa fa-plus"></i>This is home.</h1>
+    </div>
+
+    <script src="assets/jquery.js"></script>
+    <script src="bootstrap.min.js"></script>
+    <script src="assets/fontawesome.js"></script>
 </body>
 </html>
